95e49f45cacc2f96b5269a9c2e646298bd78664d
JeanMoreno13 <jeanmoreno13640@gmail.com>
2020-08-04 13:53:48 +0200

    Updates

---
+++ precache-manifest.e64b6879e47d620ae97e4a913c6c4353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578a14548332e2cd9a58eeadeba4aca3",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "09179f08cf3c3e572c82",
+    "url": "/todo-list/static/js/2.5eff9af4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.5eff9af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d973bda8134c5f3a20d6",
+    "url": "/todo-list/static/js/main.95b318b2.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
